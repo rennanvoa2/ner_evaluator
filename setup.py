601e38222ff96cde2202a60981427399cd377c8d
rennanvoa2 <rennanvoa2@lwmail.com.br>
2022-08-11 09:57:51 -0300

    Change setup

--- setup.py
+++ setup.py
@@ -1,36 +1,14 @@
-import setuptools
 from distutils.core import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="ner_evaluator",
-    version="1.0.1",
+setup(
+    name = 'ner_evaluator',
+    packages = ['ner_evaluator'],
+    version = 'v1.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = "Evaluate Named Entity Recognition (NER) models",
     author="Rennan Valadares",
     author_email="rennanvoa2@gmail.com",
-    description="Evaluate Named Entity Recognition (NER) models",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rennanvoa2/evaluate_ner_models",
-    project_urls={
-        "Bug Tracker": "https://github.com/rennanvoa2/evaluate_ner_models/issues"
-    },
-    license="None",
-    packages=["ner_evaluator"],
-    install_requires=["rapidfuzz"],
-)
-
-
-setup(
-    name = 'my_python_package',
-    packages = ['my_python_package'],
-    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
-    author = '',
-    author_email = '',
-    url = 'github package source url',
-    download_url = 'download link you saved',
-    keywords = ['tag1', 'tag2'],
+    url = 'https://github.com/rennanvoa2/',
+    download_url = 'https://github.com/rennanvoa2/ner_evaluator/archive/refs/tags/V1.0.0.tar.gz',
+    keywords = ['ner', 'named entity recognition', 'evaluation','NER evaluation'],
     classifiers = [],
 )
